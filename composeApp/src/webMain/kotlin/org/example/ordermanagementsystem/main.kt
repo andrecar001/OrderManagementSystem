81f8f3b2a15492619e274cfba8c45aa9f5c332b3
Carlos <crdandrews@gmail.com>
2026-04-16 19:31:22 -0500

    Initial Commit

---
+++ composeApp/src/webMain/kotlin/org/example/ordermanagementsystem/main.kt
@@ -0,0 +1,11 @@
+package org.example.ordermanagementsystem
+
+import androidx.compose.ui.ExperimentalComposeUiApi
+import androidx.compose.ui.window.ComposeViewport
+
+@OptIn(ExperimentalComposeUiApi::class)
+fun main() {
+    ComposeViewport {
+        App()
+    }
+}
