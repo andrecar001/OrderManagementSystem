84590abefe72612a2418a008fe793c0f863e75f2
Carlos <crdandrews@gmail.com>
2026-04-28 03:12:05 -0500

    change ui to accept warehouse as null and fix item list

--- composeApp/src/commonMain/kotlin/org/example/ordermanagementsystem/ui/components/OrderList.kt
+++ composeApp/src/commonMain/kotlin/org/example/ordermanagementsystem/ui/components/OrderList.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
@@ -71,6 +72,7 @@ fun OrderListComponent(
     selectedWarehouseFilter: Int?,
     onSelect: (Order) -> Unit,
     onWarehouseChange: (Int?) -> Unit,
+    onRefresh: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     Column(
@@ -79,6 +81,12 @@ fun OrderListComponent(
     ) {
         Row {
             Text(text = "Orders")
+            Spacer(modifier = Modifier.weight(1f).padding(16.dp))
+
+            Button(onClick = onRefresh) {
+                Text("Refresh Orders")
+            }
+
             Spacer(modifier = Modifier.weight(1f).padding(16.dp))
             labeledDropDown(
                 label = "Filter",
